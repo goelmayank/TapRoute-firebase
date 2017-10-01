fb6b9c6868885a29f8456ef96157da6f5bdaea8a
Bharat Kashyap <bharatkashyap@outlook.com>
2017-10-01 16:42:00 +0530

    Updates to destination platform function

--- functions/index.js
+++ functions/index.js
@@ -4,69 +4,6 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
-/**
- * Triggers when a user gets a new follower and sends a notification.
- *
- * Followers add a flag to `/followers/{followedUid}/{followerUid}`.
- * Users save their device notification tokens to `/users/{followedUid}/notificationTokens/{notificationToken}`.
- */
- exports.sendFollowerNotification = functions.database.ref('/followers/{followedUid}/{followerUid}').onWrite(event => {
- 	const followerUid = event.params.followerUid;
- 	const followedUid = event.params.followedUid;
-  // If un-follow we exit the function.
-  if (!event.data.val()) {
-  	return console.log('User ', followerUid, 'un-followed user', followedUid);
-  }
-  console.log('We have a new follower UID:', followerUid, 'for user:', followerUid);
-
-  // Get the list of device notification tokens.
-  const getDeviceTokensPromise = admin.database().ref(`/users/${followedUid}/notificationTokens`).once('value');
-
-  // Get the follower profile.
-  const getFollowerProfilePromise = admin.auth().getUser(followerUid);
-
-  return Promise.all([getDeviceTokensPromise, getFollowerProfilePromise]).then(results => {
-  	const tokensSnapshot = results[0];
-  	const follower = results[1];
-
-    // Check if there are any device tokens.
-    if (!tokensSnapshot.hasChildren()) {
-    	return console.log('There are no notification tokens to send to.');
-    }
-    console.log('There are', tokensSnapshot.numChildren(), 'tokens to send notifications to.');
-    console.log('Fetched follower profile', follower);
-
-    // Notification details.
-    const payload = {
-    	notification: {
-    		title: 'You have a new follower!',
-    		body: `${follower.displayName} is now following you.`,
-    		icon: follower.photoURL
-    	}
-    };
-
-    // Listing all tokens.
-    const tokens = Object.keys(tokensSnapshot.val());
-
-    // Send notifications to all tokens.
-    return admin.messaging().sendToDevice(tokens, payload).then(response => {
-      // For each message check if there was an error.
-      const tokensToRemove = [];
-      response.results.forEach((result, index) => {
-      	const error = result.error;
-      	if (error) {
-      		console.error('Failure sending notification to', tokens[index], error);
-          // Cleanup the tokens who are not registered anymore.
-          if (error.code === 'messaging/invalid-registration-token' ||
-          	error.code === 'messaging/registration-token-not-registered') {
-          	tokensToRemove.push(tokensSnapshot.ref.child(tokens[index]).remove());
-      }
-  }
-});
-      return Promise.all(tokensToRemove);
-  });
-});
-});
 
 /**
  * Triggers when a user approaches a metro station.
@@ -76,7 +13,6 @@ admin.initializeApp(functions.config().firebase);
  exports.sendFollowerNotification = functions.database.ref('/trips/{tripID}/destination_station_approaching/{stationName}').onWrite(event => {
  	const tripID = event.params.tripID;
  	const stationName = event.params.stationName;
-  // If un-follow we exit the function.
 
   // Get the list of device notification tokens.
   const getDeviceTokensPromise = admin.database().ref(`/users/${tripID}/notificationTokens`).once('value');
