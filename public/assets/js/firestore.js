1958f547449669b260f2d6c04817c8bb71ad7c71
Roboneet <neethumariyajoy@gmail.com>
2017-10-18 14:28:39 +0530

    updating

--- public/assets/js/firestore.js
+++ public/assets/js/firestore.js
@@ -9,39 +9,74 @@ var config = {
 firebase.initializeApp(config);
 var db = firebase.firestore();
 
-function save(message,collection){
+// function save(message,collection, callback = null){
 
-	db.collection(collection).add({
-	  pin:message,
-	  timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-	}).then(function(docRef) {
-	  console.log("Document written with ID: ", docRef.id);
+// 	db.collection(collection).add({
+// 	  pin:message,
+// 	  timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+// 	}).then(function(docRef) {
+// 	  console.log("Document written with ID: ", docRef.id);
+// 	  if(callback)
+// 	  	callback();
+// 	})
+// 	.catch(function(error) {
+// 	  console.error("Error adding document: ", error);
 
-	})
-	.catch(function(error) {
-	  console.error("Error adding document: ", error);
-
-	});
-}
+// 	});
+// }
 
 function set(pin,obj,collection){
-	obj = Object.assign(obj, {timestamp: firebase.firestore.FieldValue.serverTimestamp(),});
-	db.collection(collection).doc(pin).set(obj).then(function(docRef) {
-	  console.log("Document written with ID: ", docRef.id);
+	
+	checkIfExits(pin, collection).then((b)=>{
+		if(!b){
+			db.collection(collection).doc(pin).set(obj).then(function() {
+			  get(pin, collection)
+			}).catch(e => {
+				console.log(e);
+			})
+		}
+		
+		db.collection(collection).doc(pin).update(obj).then(function(){
+			get(pin, collection)
+		}).catch(e => {
+				console.log(e);
+			})
+		
 	})
-	.catch(function(error) {
-	  console.error("Error adding document: ", error);
-
-	});
+	
 }
 
 function get(pin,collection){
 
-	db.collection(collection).doc(pin).get().then(function(doc) {
+	return db.collection(collection).doc(pin).get().then(function(doc) {
 		if (doc.exists) {
-			console.log("Document data:", doc.data());
+			return doc;
+			
 		} else {
 			console.log("No such document!");
+			return null;
+		}
+	}).catch(function(error) {
+		console.log("Error getting document:", error);
+		return error
+	});
+
+}
+function getOrigin(pin, collection){
+	return get(pin, collection).then(doc=>{
+		if(!doc)return "-1";
+		return doc.data().origin?doc.data().origin:"-1";
+	})
+}
+
+function checkIfExits(pin,collection){
+
+	return db.collection(collection).doc(pin).get().then(function(doc) {
+		if (doc.exists) {
+			return doc.ref.id == pin;
+			
+		} else {
+			return null;
 		}
 	}).catch(function(error) {
 		console.log("Error getting document:", error);
