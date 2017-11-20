6c242c782f3c8b522076542b49944e8521ec2169
Roboneet <neethumariyajoy@gmail.com>
2017-11-20 18:26:23 +0530

    update journey

--- functions/index.js
+++ functions/index.js
@@ -5,32 +5,32 @@ const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
 const findJouneyModule = require("./findJourney")
-exports.updateJourney = functions.firestore
-    .document('journey/{journeyId}')
-    .onCreate((event) => {
-        const data = event.data.data();
-        const origin = data.origin;
-        const destination = data.destination;
-        try {
-            return findJouneyModule.metroSearch({ origin, destination }, function(data) {
-                console.log('inside callback :P ');
-                try {
-                    var obj = Object.assign({ message: "added" }, data)
-                    event.data.ref.set(obj, { merge: true });
+// export.updateJourney = functions.firestore
+//     .document('journey/{journeyId}')
+//     .onCreate((event) => {
+//         const data = event.data.data();
+//         const origin = data.origin;
+//         const destination = data.destination;
+//         try {
+//             return findJouneyModule.metroSearch({ origin, destination }, function(data) {
+//                 console.log('inside callback :P ');
+//                 try {
+//                     var obj = Object.assign({ message: "added" }, data)
+//                     event.data.ref.set(obj, { merge: true });
 
-                } catch (e) {
-                    console.log('eeeeeerrr', err)
-                }
-
-
-            });
-        } catch (e) {
-            console.log(e);
-        }
+//                 } catch (e) {
+//                     console.log('eeeeeerrr', err)
+//                 }
 
 
+//             });
+//         } catch (e) {
+//             console.log(e);
+//         }
 
-    });
+
+
+//     });
 
 exports.addOTP = functions.firestore
     .document('user/{userId}')
