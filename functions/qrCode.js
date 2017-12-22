f15fe2049c49c15cbec1d751a19f012310a3f4aa
thenaughtychid <f2014768@pilani.bits-pilani.ac.in>
2017-12-22 10:09:57 +0530

    qr code generation

---
+++ functions/qrCode.js
@@ -0,0 +1,53 @@
+/*This Firebase Function is responsible of returning to use app the information in following format
+{
+    "userId":"20_character_long",
+    "origin":"Name",
+    "destination":"Name",
+    "less_walking":0/1,
+    "first_mile":{
+        "mode":"bus/auto",
+        "fare":"bus/metro",
+        "start_time":"in_ms",
+        "end_time":"in_ms"
+    },
+    "transit":{
+        "num_changes":"bus/auto",
+        "num_stations":"bus/metro"
+    },
+    "last_mile":{
+        "mode":"bus/auto",
+        "fare":"bus/metro",
+        "start_time":"in_ms",
+        "end_time":"in_ms"
+    }
+}
+on receiving a RESTful request on the api endpoint /qrcode with a request body in following format
+{
+    "qrcode":"link_of_png"
+}
+*/
+
+/*===============================1. REQUIRED LIBRARIES AND CONFIGURATIONS==========================*/
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+var QRCode = require('qrcode');
+
+/*========================================END HERE=================================================*/
+
+
+exports.handler = function (req, res, database, firestore) {
+//===============================1. Store DATA and fetch the document id=========================================
+    var data = req.body;
+    
+    var qrcode_id = "";
+
+
+
+/*===============================2. create QR Code and return its image url======================================*/
+  //The following snippet is to generate the qrcode with the the journey id encrypted on it
+  QRCode.toDataURL(qrcode_id, function (err, url) {
+    console.log(url)
+  })
+}
+
+/*======================================ENDS HERE==================================================*/
